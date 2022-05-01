3ae8c6a0246282e53b5b65e4b1688466ba1936a2
wenlufang <wenlufang@tencent.com>
2022-05-01 16:53:59 +0800

    add simple cube

---
+++ Assets/Scripts/XRenderPipelineAsset.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "SRPLearn/XRendererPipelineAsset")]
+public class XRenderPipelineAsset : RenderPipelineAsset
+{
+   protected override RenderPipeline CreatePipeline() {
+       return new XRenderPipeline();
+   }
+}
+
+public class XRenderPipeline: RenderPipeline {
+    protected override void Render(ScriptableRenderContext context, Camera[] camears) {
+         foreach(var camera in camears) {
+             RenderPerCamera(context, camera);
+
+            if (camera.clearFlags == CameraClearFlags.Skybox && RenderSettings.skybox != null)
+            {
+                context.DrawSkybox(camera);
+            }
+        }
+         context.Submit();
+    }
+
+    private ShaderTagId _shaderTag = new ShaderTagId("ExampleLightModeTag");
+
+
+
+    private void RenderPerCamera(ScriptableRenderContext context, Camera camera) {
+        context.SetupCameraProperties(camera);
+        //对场景进行裁剪
+        camera.TryGetCullingParameters(out var cullingParams);
+        var cullingResults = context.Cull(ref cullingParams);
+
+        //相关参数，用来计算物体渲染时的排序
+        var sortingSetting = new SortingSettings(camera);
+        var drawSetting = new DrawingSettings(_shaderTag, sortingSetting);
+        //相关参数，用来过滤需要渲染的物体
+        var filterSetting = new FilteringSettings(RenderQueueRange.all);
+        //绘制物体
+        context.DrawRenderers(cullingResults, ref drawSetting, ref filterSetting);
+
+
+    }
+}
